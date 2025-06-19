2171370d42fced990869753fc1cb4b5cb9e95d1a
abir <sanusharma8774@gmail.com>
2025-06-19 11:35:41 +0530

    ne change

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -4,5 +4,7 @@ public class LoginPage {
 
     public static void main(String[] args) {
         System.out.println("hello this is abir");
+
+        System.out.println("hello there this is kiriti");
     }
 }
